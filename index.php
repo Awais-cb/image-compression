27fc0b9cc887b12167725d155eebbffd98834da0
Awais Fiaz <awais.fiaz@oladoc.com>
2023-10-22 22:05:12 +0500

    First commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$input_ext = "jpg";
+$output_postfix = "_output.webp";
+
+
+if (!function_exists('dd')) {
+    function dd($d)
+    {
+        die('<pre>' . print_r($d, 1));
+    }
+}
+
+if (!function_exists('p')) {
+    function p($d, $log=true)
+    {
+        if ($log) {
+            error_log(print_r($d, 1));
+        }
+        echo '<pre>';
+        print_r($d);
+        echo '</pre>';
+    }
+}
+
+
+// Get a list of all image files in the current folder
+$imageFiles = glob("*.{$input_ext}");
+// dd($imageFiles);
+// Iterate through the image files and run FFmpeg command
+foreach ($imageFiles as $imageFile) {
+    $inputFileName = $imageFile;
+    $outputFileName = str_replace(".{$input_ext}", $output_postfix, $imageFile);
+
+    // Construct and execute the FFmpeg command
+    $ffmpegCommand = "ffmpeg -y -i $inputFileName -c:v libwebp -q:v 60 $outputFileName";
+   
+    p($ffmpegCommand);
+    $output = shell_exec($ffmpegCommand);
+    p($output);
+    p("Converted $inputFileName to $outputFileName");
+    p("------------------------------------------------");
+}
+
+?>
